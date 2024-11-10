e0f121fc01943e34b5921d099d6719b5d881f8a8
nithishkarthick <nknithish0708@gmail.com>
2024-11-10 11:48:45 +0530

    version2

---
+++ src/main/java/com/telegram/App.java
@@ -0,0 +1,10 @@
+package com.telegram;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Welcome to my telegram");
+    }
+}
